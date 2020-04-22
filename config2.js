53356bfab32eafb5b2ad6a139e42b70291a41901
st4zz <59162760+st4zzc0de@users.noreply.github.com>
2020-04-23 02:59:43 +0700

    Update Config

--- config2.js
+++ config2.js
@@ -86,100 +86,102 @@ function aplayer1 () {
         theme: '#F57F17',
         lrcType: 3,
         audio: [{
-            name: 'Eye of the Storm',
+            name: 'Eye of the Storm', //one
             artist: 'ONE OK ROCK',
-            url: 'Link0',
+            url: 'https://dl.dropboxusercontent.com/s/xcfqy2cnrbqqd5s/01.%20Eye%20of%20the%20Storm.mp3',
             cover: 'https://is4-ssl.mzstatic.com/image/thumb/Music128/v4/47/df/b6/47dfb61e-3a3b-26fe-4507-ebcbd6765519/075679858412.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/eyestorm.lrc',
             theme: '#ebd0c2'
         }, {
-            name: 'Yummy',
+            name: 'Yummy', //two
             artist: 'Justin Bieber',
-            url: 'Link-0',
+            url: 'https://dl.dropboxusercontent.com/s/wpc2d6fk3ny4kev/01.%20Yummy.mp3',
             cover: 'https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/f6/55/48/f65548ea-93c8-8c8f-c1e6-d1561b192e83/19UM1IM02770.rgb.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/yummy.lrc',
             theme: '#46718b'
         }, {
-            name: 'End of Time',
+            name: 'End of Time', //three
             artist: 'K-391, Alan Walker & Ahrix',
-            url: 'Link1',
+            url: 'https://dl.dropboxusercontent.com/s/8caonq28d7ava5g/01.%20End%20of%20Time.mp3',
             cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/9a/ed/82/9aed8218-ccb9-b429-45b6-cda6aea868e1/195081074047.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/endoftime.lrc',
             theme: '#505d6b'
         }, {
-            name: 'Answer',
+            name: 'Answer', //four
             artist: 'ATEEZ',
-            url: 'Link2',
+            url: 'https://dl.dropboxusercontent.com/s/7bxind1c7lx9orr/01.%20Answer.mp3',
             cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/58/e8/c1/58e8c1eb-ea72-7d5d-f0be-31c36ec8f6cd/886448229674.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/atzanswer.lrc',
             theme: '#ebd0c2'
         }, {
-            name: 'Answer (Japanese Ver.)',
+            name: 'Answer (Japanese Ver.)', //five
             artist: 'ATEEZ',
-            url: 'Link3',
+            url: 'https://dl.dropboxusercontent.com/s/okymo4nf0jhb2wr/01.%20Answer%20%28Japanese%20Ver%29.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/de/fd/fb/defdfb06-5624-610a-4476-700910fc4816/COCP-41063.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/atzanswerjp.lrc',
             theme: '#46718b'
         }, {
-            name: 'Levanter',
+            name: 'Levanter', //six
             artist: 'Stray Kids',
-            url: 'Link4',
+            url: 'https://dl.dropboxusercontent.com/s/hcfvlbvy4cygo5r/01.%20Levanter.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/74/ee/6e/74ee6eec-8dfa-2f41-f819-25e0bc6a5fbd/SKZ_LEVANTER.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/levanter.lrc',
             theme: '#505d6b'
         }, {
-            name: 'Levanter -Japanese ver.-',
+            name: 'Levanter -Japanese ver.-', //seven
             artist: 'Stray Kids',
-            url: 'Link5',
+            url: 'https://dl.dropboxusercontent.com/s/odfzko4bfwtsgrd/01.%20Levanter-Japanese%20ver.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/74/ee/6e/74ee6eec-8dfa-2f41-f819-25e0bc6a5fbd/SKZ_LEVANTER.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/levanterjp.lrc',
             theme: '#ebd0c2'
         }, {
-            name: 'Run Away (Japanese Version)',
+            name: 'Run Away (Japanese Version)', //eight
             artist: 'TXT',
-            url: 'Link6',
+            url: 'https://dl.dropboxusercontent.com/s/6by05suzg27lt8g/01.%20Run%20Away%20%28Japanese%20Version%29.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music113/v4/9c/ef/37/9cef37ea-94d9-44d3-a748-a18c931dd892/19UM1IM06938.rgb.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/nolyric.lrc',
             theme: '#46718b'
         }, {
-            name: 'Let Me Hear',
+            name: 'Let Me Hear', //nine
             artist: 'Fear, and Loathing in Las Vegas',
-            url: 'Link7',
+            url: 'https://dl.dropboxusercontent.com/s/ny27nerpy6uzn0u/01.%20Let%20Me%20Hear.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music6/v4/e9/30/13/e930134e-988b-1bcb-b021-160eebd141c8/VPCC-82322.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/letmehear.lrc',
             theme: '#505d6b'
         }, {
-            name: 'SHINE',
+            name: 'SHINE', //ten
             artist: 'Fear, and Loathing in Las Vegas',
-            url: 'Link8',
+            url: 'https://dl.dropboxusercontent.com/s/byauwqenlixzip5/01.%20SHINE.mp3',
             cover: 'https://is4-ssl.mzstatic.com/image/thumb/Music117/v4/e6/71/0f/e6710f5d-ff92-5bf0-6ff0-62c81de5cc28/190295818739.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/shine.lrc',
             theme: '#ebd0c2'
         }, {
-            name: 'Stay With Me',
+            name: 'Stay With Me', //eleven
             artist: 'Seven Billion Dots',
-            url: 'JLink7',
+            url: 'https://dl.dropboxusercontent.com/s/9hs4frpvcds68mm/01.%20Stay%20With%20Me.mp3',
             cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/e8/ce/9d/e8ce9de9-4bb6-32dc-b3ee-c6535ec4a274/886448154143.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/nolyric.lrc',
             theme: '#46718b'
         }, {
-            name: 'Prover',
+            name: 'Prover', //twelve
             artist: 'milet',
-            url: 'JLink8',
+            url: 'https://dl.dropboxusercontent.com/s/nwkk7gct86fz8dw/01.%20Prover.mp3',
             cover: 'https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/7b/1e/63/7b1e6390-dd10-9138-5293-ee5a69f27df8/jacket_SEXX01939B01A_550.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/nolyric.lrc',
             theme: '#46718b'
         }, {
-            name: 'Moshimo Mata Itsuka',
+            name: 'Moshimo Mata Itsuka', //thirteen
             artist: '‎Ariel NOAH (ft. Ariel Nidji)',
-            url: 'Link9',
+            url: 'https://dl.dropboxusercontent.com/s/c6vmk1n76vqtfnx/01.%20Moshimo%20Mata%20Itsuka.mp3',
             cover: 'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/46/39/bc/4639bc17-1bdf-a6a9-7f37-60eafa2de4f9/dj.cvpippdo.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/moshimo.lrc',
             theme: '#46718b'
         }, {
-            name: 'End of Time0',
-            artist: 'K-391, Alan Walker & Ahrix0',
-            url: 'Link10',
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/9a/ed/82/9aed8218-ccb9-b429-45b6-cda6aea868e1/195081074047.jpg/1000x1000bb.jpg0',
-            theme: '#505d6b'
-        }, {
-            name: 'End of Time1',
-            artist: 'K-391, Alan Walker & Ahrix1',
-            url: 'Link11',
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/9a/ed/82/9aed8218-ccb9-b429-45b6-cda6aea868e1/195081074047.jpg/1000x1000bb.jpg1',
-            theme: '#46718b'
-        }, {
-            name: 'Black Catcher',
+            name: 'Black Catcher', //fourteen
             artist: 'VICKEBLANKA',
-            url: 'Link+0',
+            url: 'https://dl.dropboxusercontent.com/s/oul7t2xkq9f171i/01.%20Black%20Catcher.mp3',
             cover: 'https://is2-ssl.mzstatic.com/image/thumb/Music113/v4/7a/65/1c/7a651ca3-5cb5-e936-6c87-bf63193f8ab9/ANTCD-A0000000841.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/blcatcher.lrc',
             theme: '#505d6b'
         }]
     });
@@ -194,98 +196,100 @@ function aplayer0 () {
         audio: [{
             name: 'Black Catcher',
             artist: 'VICKEBLANKA',
-            url: 'Link+0',
+            url: 'https://dl.dropboxusercontent.com/s/oul7t2xkq9f171i/01.%20Black%20Catcher.mp3',
             cover: 'https://is2-ssl.mzstatic.com/image/thumb/Music113/v4/7a/65/1c/7a651ca3-5cb5-e936-6c87-bf63193f8ab9/ANTCD-A0000000841.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/blcatcher.lrc',
             theme: '#505d6b'
         }, {
             name: 'Yummy',
             artist: 'Justin Bieber',
-            url: 'Link-0',
+            url: 'https://dl.dropboxusercontent.com/s/wpc2d6fk3ny4kev/01.%20Yummy.mp3',
             cover: 'https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/f6/55/48/f65548ea-93c8-8c8f-c1e6-d1561b192e83/19UM1IM02770.rgb.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/yummy.lrc',
             theme: '#46718b'
-        }, {
-            name: 'End of Time1',
-            artist: 'K-391, Alan Walker & Ahrix1',
-            url: 'Link11',
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/9a/ed/82/9aed8218-ccb9-b429-45b6-cda6aea868e1/195081074047.jpg/1000x1000bb.jpg1',
-            theme: '#505d6b'
-        }, {
-            name: 'End of Time0',
-            artist: 'K-391, Alan Walker & Ahrix0',
-            url: 'Link10',
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/9a/ed/82/9aed8218-ccb9-b429-45b6-cda6aea868e1/195081074047.jpg/1000x1000bb.jpg0',
-            theme: '#ebd0c2'
         }, {
             name: 'Moshimo Mata Itsuka',
             artist: '‎Ariel NOAH (ft. Ariel Nidji)',
-            url: 'Link9',
+            url: 'https://dl.dropboxusercontent.com/s/c6vmk1n76vqtfnx/01.%20Moshimo%20Mata%20Itsuka.mp3',
             cover: 'https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/46/39/bc/4639bc17-1bdf-a6a9-7f37-60eafa2de4f9/dj.cvpippdo.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/moshimo.lrc',
             theme: '#46718b'
         }, {
             name: 'Prover',
             artist: 'milet',
-            url: 'JLink8',
+            url: 'https://dl.dropboxusercontent.com/s/nwkk7gct86fz8dw/01.%20Prover.mp3',
             cover: 'https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/7b/1e/63/7b1e6390-dd10-9138-5293-ee5a69f27df8/jacket_SEXX01939B01A_550.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/nolyric.lrc',
             theme: '#46718b'
         }, {
             name: 'Stay With Me',
             artist: 'Seven Billion Dots',
-            url: 'JLink7',
+            url: 'https://dl.dropboxusercontent.com/s/9hs4frpvcds68mm/01.%20Stay%20With%20Me.mp3',
             cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/e8/ce/9d/e8ce9de9-4bb6-32dc-b3ee-c6535ec4a274/886448154143.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/nolyric.lrc',
             theme: '#46718b'
         }, {
             name: 'SHINE',
             artist: 'Fear, and Loathing in Las Vegas',
-            url: 'Link8',
+            url: 'https://dl.dropboxusercontent.com/s/byauwqenlixzip5/01.%20SHINE.mp3',
             cover: 'https://is4-ssl.mzstatic.com/image/thumb/Music117/v4/e6/71/0f/e6710f5d-ff92-5bf0-6ff0-62c81de5cc28/190295818739.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/shine.lrc',
             theme: '#505d6b'
         }, {
             name: 'Let Me Hear',
             artist: 'Fear, and Loathing in Las Vegas',
-            url: 'Link7',
+            url: 'https://dl.dropboxusercontent.com/s/ny27nerpy6uzn0u/01.%20Let%20Me%20Hear.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music6/v4/e9/30/13/e930134e-988b-1bcb-b021-160eebd141c8/VPCC-82322.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/letmehear.lrc',
             theme: '#ebd0c2'
         }, {
             name: 'Run Away (Japanese Version)',
             artist: 'TXT',
-            url: 'Link6',
+            url: 'https://dl.dropboxusercontent.com/s/6by05suzg27lt8g/01.%20Run%20Away%20%28Japanese%20Version%29.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music113/v4/9c/ef/37/9cef37ea-94d9-44d3-a748-a18c931dd892/19UM1IM06938.rgb.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/nolyric.lrc',
             theme: '#46718b'
         }, {
             name: 'Levanter -Japanese ver.-',
             artist: 'Stray Kids',
-            url: 'Link5',
+            url: 'https://dl.dropboxusercontent.com/s/odfzko4bfwtsgrd/01.%20Levanter-Japanese%20ver.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/74/ee/6e/74ee6eec-8dfa-2f41-f819-25e0bc6a5fbd/SKZ_LEVANTER.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/levanterjp.lrc',
             theme: '#505d6b'
         }, {
             name: 'Levanter',
             artist: 'Stray Kids',
-            url: 'Link4',
+            url: 'https://dl.dropboxusercontent.com/s/hcfvlbvy4cygo5r/01.%20Levanter.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/74/ee/6e/74ee6eec-8dfa-2f41-f819-25e0bc6a5fbd/SKZ_LEVANTER.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/levanter.lrc',
             theme: '#ebd0c2'
         }, {
             name: 'Answer (Japanese Ver.)',
             artist: 'ATEEZ',
-            url: 'Link3',
+            url: 'https://dl.dropboxusercontent.com/s/okymo4nf0jhb2wr/01.%20Answer%20%28Japanese%20Ver%29.mp3',
             cover: 'https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/de/fd/fb/defdfb06-5624-610a-4476-700910fc4816/COCP-41063.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/atzanswerjp.lrc',
             theme: '#46718b'
         }, {
             name: 'Answer',
             artist: 'ATEEZ',
-            url: 'Link2',
+            url: 'https://dl.dropboxusercontent.com/s/7bxind1c7lx9orr/01.%20Answer.mp3',
             cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/58/e8/c1/58e8c1eb-ea72-7d5d-f0be-31c36ec8f6cd/886448229674.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/atzanswer.lrc',
             theme: '#505d6b'
         }, {
             name: 'End of Time',
             artist: 'K-391, Alan Walker & Ahrix',
-            url: 'Link1',
+            url: 'https://dl.dropboxusercontent.com/s/8caonq28d7ava5g/01.%20End%20of%20Time.mp3',
             cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/9a/ed/82/9aed8218-ccb9-b429-45b6-cda6aea868e1/195081074047.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/endoftime.lrc',
             theme: '#46718b'
         }, {
             name: 'Eye of the Storm',
             artist: 'ONE OK ROCK',
-            url: 'Link0',
+            url: 'https://dl.dropboxusercontent.com/s/xcfqy2cnrbqqd5s/01.%20Eye%20of%20the%20Storm.mp3',
             cover: 'https://is4-ssl.mzstatic.com/image/thumb/Music128/v4/47/df/b6/47dfb61e-3a3b-26fe-4507-ebcbd6765519/075679858412.jpg/1000x1000bb.jpg',
+            lrc: 'https://raw.githubusercontent.com/st4zzc0de/APlayer/master/lyrics/eyestorm.lrc',
             theme: '#ebd0c2'
         }]
     });
